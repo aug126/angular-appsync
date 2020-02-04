70a982a74d02920055db8ecf1a67b9012b053cc4
cedvdb <cedvandenbosch@gmail.com>
2020-02-04 16:38:28 +0100

    resolving

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject, BehaviorSubject } from 'rxjs';
-import { Product } from 'app-sync/src/models';
-import { DataStore, Predicates } from '@aws-amplify/datastore';
 
 
 
