48a0f83314a1745f0fa583952ddfc949f8d76ac0
augustin <doat.augustin@gmail.com>
2020-02-05 15:19:15 +0100

    ajout création product

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -5,18 +5,19 @@ import AWSAppSyncClient, { AUTH_TYPE } from "aws-appsync";
 
 import awsconfig from "../app-sync/src/aws-exports";
 
-import * as queries from "../app-sync/src/graphql/queries"
-import { Auth } from 'aws-amplify';
+import * as queries from "../app-sync/src/graphql/queries";
+import * as mutations from "../app-sync/src/graphql/mutations";
+import { Auth } from "aws-amplify";
 
 const client = new AWSAppSyncClient({
   url: awsconfig.aws_appsync_graphqlEndpoint,
   region: awsconfig.aws_appsync_region,
   auth: {
     type: AUTH_TYPE.AMAZON_COGNITO_USER_POOLS, // authentication type in awsconfig.aws_appsync_authenticationType,
-    jwtToken: async () => (await Auth.currentSession()).getIdToken().getJwtToken(),
+    jwtToken: async () =>
+      (await Auth.currentSession()).getIdToken().getJwtToken()
   }
 });
-
 
 @Component({
   selector: "app-test",
@@ -37,6 +38,25 @@ export class TestComponent implements OnInit {
     client
       .query({
         query: gql([queries.listProducts])
+      })
+      .then(data => {
+        console.timeEnd("testListProductQuery");
+        console.log(data);
+      });
+  }
+
+  testCreateSimpleProduct() {
+    console.time("testListProductQuery");
+
+    client
+      .mutate({
+        mutation: gql([mutations.createProduct]),
+        variables: {
+          input: {
+            name: "Test Product Mutation",
+            supplierName: "supplier"
+          }
+        }
       })
       .then(data => {
         console.timeEnd("testListProductQuery");
